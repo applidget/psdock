736af26aa32aaa798586af0335e398f6ea3e5058
Emeric Fatih <emericfatih@orange.fr>
2014-07-22 17:38:19 +0200

    Corrected some bugs in context.go

--- context.go
+++ context.go
@@ -1,18 +1,19 @@
 package psdock
 
 import (
-	"exec"
+	"log"
 	"os"
+	"os/exec"
 	"strings"
 )
 
 //SetEnvVars sets the environment variables for the launched process
-func (c *Cmd) SetEnvVars(envVars string) {
+func SetEnvVars(c *exec.Cmd, envVars string) {
 	if envVars == "" {
 		//We don't want to export any environment variable
 		//But we can't leave c.Env empty, as exec.Run will export all the current
 		//env vars. So we only export one unused environment variable
-		c.Env.append("UNUSED")
+		c.Env = append(c.Env, "UNUSED")
 	}
 	for _, str := range strings.Split(envVars, " ") {
 		//We want to export all the psdock's environment variables to the launched
@@ -25,7 +26,7 @@ func (c *Cmd) SetEnvVars(envVars string) {
 			} else {
 				//We have new environment variables to export.
 				//We have to manually copy all the current environment variables to c.Env
-				c.Env = append(c.Env, os.Environ)
+				c.Env = append(c.Env, os.Environ()...)
 			}
 			//We want to export a specific variable from the current environment
 		} else if str[0] == '$' {
@@ -33,11 +34,11 @@ func (c *Cmd) SetEnvVars(envVars string) {
 			if valueOfEnv == "" {
 				log.Fatal("The variable", str[1:], "is not a defined environment variable")
 			} else {
-				c.Env.append([]string{str[1:], "=", valueOfEnv})
+				c.Env = append(c.Env, strings.Join([]string{str[1:], "=", valueOfEnv}, ""))
 			}
 			//str is already in the form "VAR1=value1"
 		} else {
-			c.Env.append(str)
+			c.Env = append(c.Env, str)
 		}
 	}
 }
