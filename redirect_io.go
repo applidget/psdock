253f1fefa0212b920327d41a9c353fab99b2aece
Emeric Fatih <emericfatih@orange.fr>
2014-07-25 12:17:36 +0200

    Started working on log redirection

--- redirect_io.go
+++ redirect_io.go
@@ -1,12 +1,19 @@
 package psdock
 
 import (
+	"bufio"
 	"io"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func redirectIO(cmd *exec.Cmd, f *os.File) {
-	var w io.Writer = os.Stdout
-	io.Copy(w, f)
+	go io.Copy(f, os.Stdin)
+	scanner := bufio.NewScanner(f)
+	//_ = scanner.Scan()
+	for scanner.Scan() {
+		io.Copy(os.Stdout, strings.NewReader(scanner.Text()))
+	}
+	//io.Copy(os.Stdout, f)
 }
