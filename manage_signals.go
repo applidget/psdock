c7f513a8edf6910eabf9fe2775b1346449d4b135
Emeric Fatih <emericfatih@orange.fr>
2014-08-04 11:55:38 +0200

    Now redirects almost all the signals received to the child

--- manage_signals.go
+++ manage_signals.go
@@ -9,9 +9,22 @@ import (
 //ManageSignals awaits for incoming signals and triggers a http request when one
 //is received. Signals listened to are SIGINT, SIGQUIT, SIGTERM, SIGHUP, SIGALRM and SIGPIPE
 func ManageSignals(p *Process) {
-	signalChannel := make(chan os.Signal)
-	signal.Notify(signalChannel, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGHUP, syscall.SIGALRM, syscall.SIGPIPE)
-	_ = <-signalChannel
+	termSignalChannel := make(chan os.Signal)
+	otherSignalChannel := make(chan os.Signal)
+	signal.Notify(termSignalChannel, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGHUP, syscall.SIGALRM, syscall.SIGPIPE)
+	otherSignals := []os.Signal{syscall.SIGABRT, syscall.SIGBUS, syscall.SIGCHLD, syscall.SIGCONT, syscall.SIGEMT, syscall.SIGFPE,
+		syscall.SIGILL, syscall.SIGIO, syscall.SIGIOT, syscall.SIGPROF, syscall.SIGSEGV, syscall.SIGSTOP, syscall.SIGSYS,
+		syscall.SIGTRAP, syscall.SIGTSTP, syscall.SIGTTIN, syscall.SIGTTOU, syscall.SIGURG, syscall.SIGUSR1,
+		syscall.SIGUSR2, syscall.SIGVTALRM, syscall.SIGWINCH, syscall.SIGXCPU, syscall.SIGXFSZ}
+
+	signal.Notify(otherSignalChannel, otherSignals...)
+	go func() {
+		for {
+			s := <-otherSignalChannel
+			p.Cmd.Process.Signal(s)
+		}
+	}()
+	_ = <-termSignalChannel
 
 	//Terminate the process and notify
 	p.eofChannel <- true
